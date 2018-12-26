2ac51d361703ad2ca50b8426fd89b7d8f6073a82
Arina Safiullina <31733496+arinasaf11@users.noreply.github.com>
2018-12-26 23:05:13 +0300

    Add files via upload

---
+++ ISR/json_in_table.py
@@ -0,0 +1,46 @@
+import json
+
+"""Функция для считывания json- данных из файла"""
+def json_file_reader(file_name):
+    try:
+        file = open(file_name, 'r')
+        data = json.loads(file.read()) #json.loads - считывание строки в формате JSON в объект Python
+        file.close()
+    except FileNotFoundError:
+        data='File not found'
+    return data
+
+"""функция для вывода данных в виде таблицы"""
+def printJson(data):
+    print( #выводим большую строку с графами нашей таблицы
+		'Name'.ljust(16) +    #ljust - дополняет строку до указанной длины указанным символом.
+		'Surname'.ljust(16) +
+		'Departure point'.ljust(16) +
+		'Landing point'.ljust(16)
+	)
+    print('-'*16*4)
+    for i in data["passengers"]: 
+        print(
+		 str(i["Name"]).ljust(16) + 
+		 str(i["Surname"]).ljust(16) +
+		 str(i["Departure point"]).ljust(16) +
+		 str(i["Landing point"]).ljust(16)
+	    )
+
+file_name = 'passengers.json'
+
+
+
+
+"""Cчитывание json-файла с использованием менеджера контекстов"""
+
+with open(file_name, 'r') as f:
+    try:
+        data = json.loads(file)
+    except JSONDecodeError:
+        print("Not a JSON file")
+    else:
+        print(data)
+
+printJson(json_file_reader(file_name))
+
